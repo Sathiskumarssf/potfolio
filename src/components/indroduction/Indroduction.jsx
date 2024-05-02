c15528a1fc45aae0fc00621a521557f62e2283f9
Educative Learner <sathiskumarsk2001@gmail.com>
2024-05-02 21:39:36 +0530

    Initial commit

---
+++ src/components/indroduction/Indroduction.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './indroduction.css'
+import Profilepicture from '../../assets/IMG-20230904-WA0027.jpg'
+
+const Indroduction = () => {
+  return (
+    <div className='main-conatiner1'> 
+         
+         <div className="item1">
+             <img src={Profilepicture}  alt="" srcset=""  className='image1 p-5'/>
+             <h1>
+              (Web && mobile developer)
+             </h1>
+         </div>
+         <div className="item"   style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1 style={{color:'white'}}>
+              I'm sathis <br />
+              Software Engineer <br />
+              undergraduate.my GPA is 3.6
+            </h1>
+
+            <h3  style={{color:'white'}}>
+            learned lot of technologies base on Web devolopment, <br />
+             Mobile devolopment,Data science and Artificial interlegent
+            </h3>
+
+            <button className='btn btn-success' >Hire Me</button>
+        </div>
+         
+    </div>
+  )
+}
+
+export default Indroduction
+
+ 
+ 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FaGithub, FaLinkedin, FaHome, FaEnvelope } from 'react-icons/fa'; // Remove '../../node_modules/' from the import path
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='p-3'>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Menu</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#"><FaHome /> Home</a> {/* Use <FaHome /> instead of {FaHome} */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><FaGithub /> Github</a> {/* Use <FaGithub /> instead of {FaGithub} */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><FaLinkedin /> LinkedIn</a> {/* Use <FaLinkedin /> instead of {FaLinkedin} */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link  " aria-disabled="true"><FaEnvelope/>contect</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
